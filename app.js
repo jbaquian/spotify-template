4fec27e68c5dab985571914ec0ddc1314468f4d7
jacob <jlb959@uw.edu>
2015-11-12 10:36:33 -0800

    First update

--- app.js
+++ app.js
@@ -1,27 +1,41 @@
 var data;
-var baseUrl = 'https://api.spotify.com/v1/search?type=track&query='
+var baseUrl = 'https://api.spotify.com/v1/search?type=artist&query='
 var myApp = angular.module('myApp', [])
 
 var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
-  $scope.audioObject = {}
-  $scope.getSongs = function() {
-    $http.get(baseUrl + $scope.track).success(function(response){
-      data = $scope.tracks = response.tracks.items
-      
+  $scope.getArtists = function() {
+    $http.get(baseUrl + $scope.artist).success(function(response){
+      data = $scope.artists = response.artists.items
     })
   }
-  $scope.play = function(song) {
-    if($scope.currentSong == song) {
-      $scope.audioObject.pause()
-      $scope.currentSong = false
-      return
-    }
-    else {
-      if($scope.audioObject.pause != undefined) $scope.audioObject.pause()
-      $scope.audioObject = new Audio(song);
-      $scope.audioObject.play()  
-      $scope.currentSong = song
-    }
+  $scope.check = function(artist) {
+    //This searches Spotify for all the albums the passed in artist is matched with
+    $http.get('https://api.spotify.com/v1/search?q=artist:'+artist.name+'&type=album').success(function(response){
+      data2=$scope.albums = response.albums.items
+    var count = 0;
+    var str = '';
+      //this loops through all of the artist matched albums which corresponds to the value passed to the function
+      $.each(data2, function(key, value) {
+        //this searches for all the tracks on the album passed through
+        $http.get('https://api.spotify.com/v1/search?q=artist:'+artist.name+'%20album:'+value.name+'&type=track').success(function(response){
+          data3=$scope.tracks=response.tracks.items
+          count = 0;
+          $.each(data3, function(key, value){
+            //this looks at whether a specific song is considered explicit and if it is will be counted
+            if(value.explicit == true){
+              count++
+            }
+          })
+          if(count >0){
+            str = value.name + ' is not safe for children, this album has ' +  count +' explicit song(s)'
+          } else {
+            str = value.name +' is safe for children'
+          }
+          //this sends an alert to the user telling how many or little explicit songs are on a specific album
+          alert(str)
+        })
+      })
+    })
   }
 })
 
